7d75ff88d795939f7d1518e23921d61f231ad73e
hilmi <muhammadhilmi92101@gmail.com>
2024-07-16 14:37:39 +0800

    basic setup

---
+++ src/app/users/page.tsx
@@ -0,0 +1,26 @@
+import { User, columns } from './columns'
+import { DataTable } from './data-table'
+
+async function getUsers(): Promise<User[]> {
+    const res = await fetch(
+        'https://669612e60312447373c0f171.mockapi.io/api/users'
+    )
+    const data = await res.json()
+    return data
+}
+const UsersPage = async () => {
+
+    const data = await getUsers()
+    
+    return (
+        <section>
+            <div className="container">
+                <h1 className='text-3xl font-bold'>All Users</h1>
+
+                <DataTable columns={columns} data={data} />
+            </div>
+        </section>
+    )
+}
+
+export default UsersPage  
